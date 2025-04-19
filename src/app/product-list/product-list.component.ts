2be942fd4fe17463c1546b9ab5ac2aec3d950d93
fer <yanobis995@gmail.com>
2025-04-19 02:13:42 +0200

    Accessing child components

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, viewChild } from '@angular/core';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
@@ -8,16 +8,22 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
   products: Product[] = [
     { id: 1, title: 'Keyboard' },
     { id: 2, title: 'Microphone' },
     { id: 3, title: 'Web camera' },
     { id: 4, title: 'Tablet' },
   ];
-  selectedProduct: Product | undefined = this.products[0];
+  selectedProduct: Product | undefined;
+
+  productDetail = viewChild(ProductDetailComponent);
 
   onAdded(product: Product) {
     alert(`${product.title} added to the cart!`);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.productDetail()!.product());
   }
 }
